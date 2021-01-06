be381209fce5b4a705456b4f7f7eea86e86cbe47
waka-syu <hamutailang059@gmail.com>
2021-01-06 14:20:03 +0900

    ユーザー新規登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-  end
+  
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角の英数字を含めて設定してください'
 end
