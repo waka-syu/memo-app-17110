83b5ffc07ef2205e26de10ce8f3cfd3b433abe95
waka-syu <hamutailang059@gmail.com>
2021-01-06 11:25:46 +0900

    ヘッダー作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :name 
-    validates :job_id, umericality: { other_than: 1 }
+    validates :job_id, numericality: { other_than: 1 }
   end
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角の英数字を含めて設定してください'
